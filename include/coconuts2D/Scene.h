448e3b56a1d46a95afad562ebd82eafa7d445595
filhoDaMain <temprilho93@gmail.com>
2025-01-13 19:46:38 +0000

    Initial commit

---
+++ include/coconuts2D/Scene.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 Andre Temprilho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstdint>
+#include <string>
+
+namespace coconuts2D {
+
+class Scene
+{
+public:
+    Scene();
+    ~Scene();
+
+    void Init(void);
+    void Run(void);
+    void End(void);
+
+private:
+    uint16_t m_ID;
+    std::string m_Name;
+    bool m_Isrunning;
+};
+
+}
