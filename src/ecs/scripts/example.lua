f564687a4fb5f7eca3c469e138bb59c0246df301
filhoDaMain <temprilho93@gmail.com>
2025-02-13 23:42:39 +0000

    Update lua example
    + Show how to get a component from lua

--- src/ecs/scripts/example.lua
+++ src/ecs/scripts/example.lua
@@ -2,10 +2,23 @@ local this = {}
 
 function this:init()
     print("init() - id: " .. self.id())
+
+    if self.registry:has(self.id(), TagComponent) then
+        tagComponent = self.registry:get(self.id(), TagComponent)
+        print("  Hello:  " .. tagComponent.tag)
+
+        -- Update its tag
+        tagComponent.tag = "Luís Vaz de Camões"
+    end
 end
 
 function this:update(time)
     print("update() - id: " .. self.id() .. " with arg: " .. time)
+
+    if self.registry:has(self.id(), TagComponent) then
+        tagComponent = self.registry:get(self.id(), TagComponent)
+        print("  Hello again:  " .. tagComponent.tag)
+    end
 end
 
 function this:destroy()
