6f26339150de0cd8d3b5043ab0de8e7e89dab8c4
filhoDaMain <temprilho93@gmail.com>
2025-02-14 00:31:11 +0000

    Update example.lua to use the core_api

--- src/ecs/scripts/example.lua
+++ src/ecs/scripts/example.lua
@@ -1,25 +1,31 @@
+local core = require("coconuts2D.core_api")
+local components = core.components
+
 local this = {}
+
 
 function this:init()
     print("init() - id: " .. self.id())
 
-    if self.registry:has(self.id(), TagComponent) then
-        tagComponent = self.registry:get(self.id(), TagComponent)
-        print("  Hello:  " .. tagComponent.tag)
+    if core.has_component(self, components.TagComponent) then
+        tagComponent = core.get_component(self, components.TagComponent)
+        print("  Hello " .. tagComponent.tag)
 
         -- Update its tag
         tagComponent.tag = "Luís Vaz de Camões"
     end
 end
 
+
 function this:update(time)
     print("update() - id: " .. self.id() .. " with arg: " .. time)
 
-    if self.registry:has(self.id(), TagComponent) then
-        tagComponent = self.registry:get(self.id(), TagComponent)
-        print("  Hello again:  " .. tagComponent.tag)
+    if core.has_component(self, components.TagComponent) then
+        tagComponent = core.get_component(self, components.TagComponent)
+        print("  Hello " .. tagComponent.tag)
     end
 end
+
 
 function this:destroy()
     print("destroy() - id: " .. self.id())
