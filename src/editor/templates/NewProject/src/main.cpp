206d1c85f47fa96922e80a824805e9e528bd6acf
filhoDaMain <temprilho93@gmail.com>
2026-02-04 20:55:01 +0000

    NewProject main.cpp: update scripting api path
    this is temporary just for myself to quickly prototype,
    no filesystem filepaths shall be used here

--- src/editor/templates/NewProject/src/main.cpp
+++ src/editor/templates/NewProject/src/main.cpp
@@ -11,13 +11,11 @@
 int main(int argc, char* argv[])
 {
     // Set LUA_PATH to include Coconuts2D Lua API scripts
-    std::string lua_api_dir("/Users/temprilho/dev/Coconuts2D/scripting");
+    std::string lua_api_dir("/Users/temprilho/dev/Coconuts2D/src/scripting/");
     std::string env_lua_path = "$LUA_PATH;" + lua_api_dir + "/?.lua";
     (void) setenv("LUA_PATH", env_lua_path.c_str(), 1);
 
     coconuts2D::Application app( static_cast<std::string>(argv[0]));
-    coconuts2D::ResourceManager rm;
-    rm.LoadVirtualFS();
 
     app.Play();
     return 0;
